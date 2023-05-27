087da2193cf62a9d739efca84e1a9f9e3d739968
Kidebazelno <b09902036@csie.ntu.edu.tw>
2023-05-28 00:33:13 +0800

    Feat: Add Rollcall service method
    I made mistake for not adding this file to the last commit

--- Service/RollcallService.py
+++ Service/RollcallService.py
@@ -1,21 +1,24 @@
-import Repository.IBaseRepository as repo
-import Model
+import Repository.BaseRepository as repo
+from Model.CoursekeyVerify import CoursekeyVerify
+from Model.StudentCheckin import StudentCheckin
 import random
 import string
 from datetime import datetime
 
 class Rollcall:
     def __init__(self) -> None:
-        self.repo =  repo.IBaseRepository(Model.CoursekeyVerify)
+        self.repo =  repo.BaseRepository(CoursekeyVerify)
     def StartRollcall(self,duration,owner):
         # return CourseKey
         # generate courseKey, startTime, duration
         courseKey = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(10))
         startTime = datetime.timestamp(datetime.now())
-        instance = Model.CoursekeyVerify(courseKey,str(startTime),str(startTime+duration),owner)
+        instance = CoursekeyVerify(courseKey,str(startTime),str(startTime+duration),owner)
         self.repo.Create(instance)
         return courseKey
-    def GetStudentList(self):
-
-        pass
+    def GetStudentList(self,courseKey:str):
+        ret:list[StudentCheckin] = repo.BaseRepository(StudentCheckin).Read(courseKey)
+        return ret
+        
+        
     
