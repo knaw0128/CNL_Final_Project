1b84c0cb075ea366ae7757a1868c1ae0e83af29e
許庭碩 <teisoku@Macintosh-6.local>
2023-05-28 18:39:18 +0800

    account service update

--- Service/AccountService.py
+++ Service/AccountService.py
@@ -1,23 +1,28 @@
 import Repository.BaseRepository as repo
+from Model.UserAccount import UserAccount
 
 class AccountService:
     def __init__(self) -> None:
         self.repo = repo.BaseRepository(UserAccount)
 
-    def register(self, username, password):
+    def Register(self, username, password):
         # input: username, password
         # output: baserepo create
+        instance = UserAccount(username, password)
+        self.repo.Create(instance)
 
-        pass
-
-    def login(self, username, password):
+    def Login(self, username, password):
         # input: username, password
         # send username to baserepo read
         # it returns the correct password
         # this function checks it's correct or not
-        pass
+        instance = self.repo.Read(username)
+        if instance.password == password:
+            return True
+        else:
+            return False 
 
-    def logout(self, username):
+    def Logout(self, username):
         # input: username
         # clean the cookie
         pass
