689dc18edf7e1567549d04467fa1fd99d7ad5199
許庭碩 <teisoku@Macintosh-6.local>
2023-05-31 22:34:44 +0800

    add oauth

--- Service/GoogleOAuthService.py
+++ Service/GoogleOAuthService.py
@@ -4,6 +4,6 @@ class GoogleOAuthService(BaseOAuthService):
     def __init__(self):
         pass
 
-    def InsertStudentInfo(self, token, coursekey):
+    def InsertStudentInfo(self, access_token, status):
         # I should extract student id from the token
         return super().InsertStudentInfo(token, coursekey)
